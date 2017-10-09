e3d4759e82f222f8cd4a824658bfc400bf35807d
Chiilog <purgexpurge@gmail.com>
2017-10-09 15:25:47 +0900

    typo fixed

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require( 'gulp' ),
-	ejs = require( 'gulp-ejs' );
-
-/* EJS */
-gulp.task( 'ejs', function () {
-	gulp.src( ['develop/ejs/**/*.ejs', '!develop/ejs/**/_*.ejs'] )
-		.pipe( ejs({}, {}, {ext: '.html'}) )
-		.pipe( gulp.dest( './develop' ) );
-});
